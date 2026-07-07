531080beb63b6e0c7bf146880fcbdbcc2a3fe6c9
drdoombg <dr.doom.bg@gmail.com>
2026-07-07 17:03:31 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+// Simple offline-first service worker for Sample Tracker.
+// Bump CACHE version when you update index.html so phones pick up the new version.
+const CACHE = 'sample-tracker-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Network-first for the page so updates show up; fall back to cache offline.
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then((r) => r || caches.match('./index.html')))
+  );
+});
